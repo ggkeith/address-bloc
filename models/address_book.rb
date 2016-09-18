217fc898cdec99b692f924d5d58cb15cc9933438
George Keith <ggkeithv@gmail.com>
2016-09-17 22:03:44 -0500

    checkpoint 19 models: completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,3 +1,20 @@
-class AddressBook
+require_relative 'entry'
 
+class AddressBook
+  attr_reader :entries
+
+  def initialize
+    @entries = []
+  end
+
+  def add_entry(name, phone_number, email)
+    index = 0
+    entries.each do |entry|
+      if name < entry.name
+        break
+      end
+      index += 1
+    end
+    entries.insert(index, Entry.new(name, phone_number, email))
+  end
 end
